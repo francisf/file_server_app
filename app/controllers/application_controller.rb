a87464aef601b752dda92d732de1fe6e46d539bd
francisf <francis@browserstack.com>
2017-11-01 13:26:40 +0530

    Basic check for user signed in debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: 'Hello, World!!!'
+    msg = "#{user_signed_in? ? "Hello, #{current_user.email}" : "Please sign_in"}"
+    render html: msg
   end
 end
